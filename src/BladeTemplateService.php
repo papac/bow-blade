6c405d97c44f8174342845cd98d7c456693845b0
Franck Dakia <dakiafranck@gmail.com>
2017-08-02 21:22:36 +0000

    Ajout de service interne

---
+++ src/BladeTemplateService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use Bow\View\View;
+use Bow\Application\Configuration;
+use Bow\Application\Services as BowService;
+
+class BladeTemplateService extends BowService
+{
+    /**
+     * Démarre le serivce
+     */
+    public function start()
+    {
+        View::singleton()->setEngine('blade');
+    }
+
+    /**
+     * @param Configuration $config
+     */
+    public function make($config)
+    {
+        View::configure($config);
+        $view = View::singleton();
+        $view->pushEngine('blade', \Papac\Blade::class);
+    }
+}

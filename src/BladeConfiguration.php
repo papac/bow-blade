e6288b87fc2cd1470fa91999312232c36c226c63
Franck DAKIA <dakiafranck@gmail.com>
2020-05-23 17:04:37 +0000

    change: formating

--- src/BladeConfiguration.php
+++ src/BladeConfiguration.php
@@ -3,7 +3,7 @@
 namespace Papac;
 
 use Bow\View\View;
-use Bow\Configuration\Loader;
+use Bow\Configuration\Loader as Config;
 use Bow\Configuration\Configuration;
 
 class BladeConfiguration extends Configuration
@@ -11,7 +11,7 @@ class BladeConfiguration extends Configuration
     /**
      * @inheritdoc
      */
-    public function create(Loader $config)
+    public function create(Config $config)
     {
         $this->container->bind('view', function () use ($config) {
             View::pushEngine('blade', BladeEngine::class);
